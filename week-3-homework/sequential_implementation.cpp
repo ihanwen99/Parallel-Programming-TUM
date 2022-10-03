07b7522f3afb5d0a881eea1f9a440f64e76c61ed
Hanwen <hanwen.liu@tum.de>
2022-10-03 21:01:07 +0200

    week-3-homework

--- week-3-homework/sequential_implementation.cpp
+++ week-3-homework/sequential_implementation.cpp
@@ -21,7 +21,8 @@ const double VIEW_Y1 = +2;
 int mandelbrot_draw(int x_resolution, int y_resolution, int max_iter,
                     double x_stepsize, double y_stepsize,
                     int palette_shift, unsigned char *img,
-                    double power, bool no_output) {
+                    double power, bool no_output)
+{
     using namespace std::complex_literals;
 
     double y;
@@ -35,8 +36,10 @@ int mandelbrot_draw(int x_resolution, int y_resolution, int max_iter,
     int pointsInSetCount = 0;
 
     // For each pixel in the image
-    for (int i = 0; i < y_resolution; i++) {
-        for (int j = 0; j < x_resolution; j++) {
+    for (int i = 0; i < y_resolution; i++)
+    {
+        for (int j = 0; j < x_resolution; j++)
+        {
             y = VIEW_Y1 - i * y_stepsize;
             x = VIEW_X0 + j * x_stepsize;
 
@@ -47,7 +50,8 @@ int mandelbrot_draw(int x_resolution, int y_resolution, int max_iter,
 
             // Apply the Mandelbrot calculation until the absolute value >= 2 (meaning the calculation will diverge to
             // infinity) or the maximum number of iterations was reached.
-            do {
+            do
+            {
                 Z = std::pow(Z, power) + C;
                 k++;
             } while (std::abs(Z) < 2 && k < max_iter);
@@ -55,14 +59,19 @@ int mandelbrot_draw(int x_resolution, int y_resolution, int max_iter,
             // If the maximum number of iterations was reached then this point is in the Mandelbrot set and we color it
             // black. Else, it is outside and we color it with a color that corresponds to how many iterations there
             // were before we confirmed the divergence.
-            if (k == max_iter) {
+            if (k == max_iter)
+            {
                 pointsInSetCount++;
             }
 
-            if(!no_output) {
-                if (k == max_iter) {
+            if (!no_output)
+            {
+                if (k == max_iter)
+                {
                     memcpy(&img[OFFSET(i, j, 0)], black, 3);
-                } else {
+                }
+                else
+                {
                     int index = (k + palette_shift) % (sizeof(colors) / sizeof(colors[0]));
                     memcpy(&img[OFFSET(i, j, 0)], colors[index], 3);
                 }
@@ -73,8 +82,8 @@ int mandelbrot_draw(int x_resolution, int y_resolution, int max_iter,
     return pointsInSetCount;
 }
 
-
-int main(int argc, char **argv) {
+int main(int argc, char **argv)
+{
     double power = 1.0;
     int max_iter = sizeof(colors) / sizeof(colors[0]);
     int x_resolution = 1511;
@@ -88,51 +97,55 @@ int main(int argc, char **argv) {
 
     // This option parsing is not very interesting.
     int c;
-    while ((c = getopt(argc, argv, "t:p:i:r:v:n:f:")) != -1) {
-        switch (c) {
-            case 'p':
-                if (sscanf(optarg, "%d", &palette_shift) != 1)
-                    goto error;
-                break;
+    while ((c = getopt(argc, argv, "t:p:i:r:v:n:f:")) != -1)
+    {
+        switch (c)
+        {
+        case 'p':
+            if (sscanf(optarg, "%d", &palette_shift) != 1)
+                goto error;
+            break;
 
-            case 'i':
-                if (sscanf(optarg, "%d", &max_iter) != 1)
-                    goto error;
-                break;
+        case 'i':
+            if (sscanf(optarg, "%d", &max_iter) != 1)
+                goto error;
+            break;
 
-            case 'r':
-                if (sscanf(optarg, "%dx%d", &x_resolution, &y_resolution) != 2)
-                    goto error;
-                break;
-            case 'n':
-                if (sscanf(optarg, "%d", &no_output) != 1)
-                    goto error;
-                break;
-            case 'f':
-                strncpy(file_name, optarg, sizeof(file_name) - 1);
-                break;
+        case 'r':
+            if (sscanf(optarg, "%dx%d", &x_resolution, &y_resolution) != 2)
+                goto error;
+            break;
+        case 'n':
+            if (sscanf(optarg, "%d", &no_output) != 1)
+                goto error;
+            break;
+        case 'f':
+            strncpy(file_name, optarg, sizeof(file_name) - 1);
+            break;
 
-            case '?':
-            error:
-                fprintf(stderr,
-                        "Usage:\n"
-                        "-i \t maximum number of iterations per pixel\n"
-                        "-r \t image resolution to be computed\n"
-                        "-p \t shift palette to change colors\n"
-                        "-f \t output file name\n"
-                        "-n \t no output(default: 0)\n"
-                        "\n"
-                        "Example:\n"
-                        "%s -r 720x480 -i 5000 -f mandelbrot.ppm\n",
-                        argv[0]);
-                exit(EXIT_FAILURE);
-                break;
+        case '?':
+        error:
+            fprintf(stderr,
+                    "Usage:\n"
+                    "-i \t maximum number of iterations per pixel\n"
+                    "-r \t image resolution to be computed\n"
+                    "-p \t shift palette to change colors\n"
+                    "-f \t output file name\n"
+                    "-n \t no output(default: 0)\n"
+                    "\n"
+                    "Example:\n"
+                    "%s -r 720x480 -i 5000 -f mandelbrot.ppm\n",
+                    argv[0]);
+            exit(EXIT_FAILURE);
+            break;
         }
     }
 
     FILE *file = nullptr;
-    if (!no_output) {
-        if ((file = fopen(file_name, "w")) == NULL) {
+    if (!no_output)
+    {
+        if ((file = fopen(file_name, "w")) == NULL)
+        {
             perror("fopen");
             exit(EXIT_FAILURE);
         }
@@ -141,12 +154,12 @@ int main(int argc, char **argv) {
     // This is a pseudo 3d (1d storage) array that can be accessed using the OFFSET Macro.
     auto *image = static_cast<unsigned char *>(malloc(x_resolution * y_resolution * sizeof(unsigned char[3])));
 
-
     // Calculate the step size.
     x_stepsize = (VIEW_X1 - VIEW_X0) / (1.0 * x_resolution);
     y_stepsize = (VIEW_Y1 - VIEW_Y0) / (1.0 * y_resolution);
 
-    if(x_stepsize <= 0 || y_stepsize <= 0) {
+    if (x_stepsize <= 0 || y_stepsize <= 0)
+    {
         printf("Please specify a valid range.\n");
         exit(-1);
     }
@@ -158,7 +171,7 @@ int main(int argc, char **argv) {
             "Max. iterations: %d\n"
             "Resolution: %dx%d\n"
             "View frame: [%lf,%lf]x[%lf,%lf]\n"
-            "Stepsize x = %lf y = %lf\n", 
+            "Stepsize x = %lf y = %lf\n",
             max_iter, x_resolution, y_resolution,
             VIEW_X0, VIEW_X1, VIEW_Y0, VIEW_Y1,
             x_stepsize, y_stepsize);
@@ -175,14 +188,17 @@ int main(int argc, char **argv) {
                                           x_stepsize, y_stepsize, palette_shift, image, power, no_output);
     outputSolution(numSamplesInSet);
 
-    if (!no_output) {
-        if (fprintf(file, "P6\n%d %d\n%d\n", x_resolution, y_resolution, 255) < 0) {
+    if (!no_output)
+    {
+        if (fprintf(file, "P6\n%d %d\n%d\n", x_resolution, y_resolution, 255) < 0)
+        {
             std::perror("fprintf");
             exit(EXIT_FAILURE);
         }
         size_t bytes_written = fwrite(image, 1,
                                       x_resolution * y_resolution * sizeof(unsigned char[3]), file);
-        if (bytes_written < x_resolution * y_resolution * sizeof(char[3])) {
+        if (bytes_written < x_resolution * y_resolution * sizeof(char[3]))
+        {
             std::perror("fwrite");
             exit(EXIT_FAILURE);
         }
